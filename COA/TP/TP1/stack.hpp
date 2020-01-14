aa849c44ef77de453ab0d3dc2d47a4c8d5895976
GregPhi <gregoire.philippot@laposte.net>
2020-01-14 13:42:30 +0100

    coa-tp1

--- COA/TP/TP1/stack.hpp
+++ COA/TP/TP1/stack.hpp
@@ -14,11 +14,15 @@ public:
     if(pile == {}){
       return EmptyExc;
     }else{
-      
+
     }
-  }; // returns the element at the top
+  }; // returns the element at the top --> renvoi le dernier élément de la liste
   void pop(); // removes element from the top
-  void push(int elem); // puts an element on top
+  void push(int elem); // puts an element on top --> insère un élément après le dernier
+  // si on est arrivé à la taille maximale :
+    //1. un nouveau tableau, plus grand, est crée ;
+    //2. le contenu du vieux tableau est copié dans le nouveau ;
+    //3. le vieux tableau est détruit.
   void clear(); // removes all elements
   int size() const{
     int cpt = 0;
