6f1e786fb882eb46727a331997d3878481884909
Philippot Gregoire <philippot@a14p18.fil.univ-lille1.fr>
2020-01-15 10:19:59 +0100

    tp

---
+++ COA/TP/TP1/README.md
@@ -0,0 +1,27 @@
+# Instructions
+
+Remplir le fichier avec les informations demandé. 
+
+# Noms
+
+	TODO
+
+# Description du travail:
+
+	TODO
+
+## Question 1
+
+	TODO
+
+## Question 2
+
+	TODO
+
+## Question 3
+
+	TODO
+
+## Question 4
+
+	TOD
---
+++ COA/TP/TP1/Stack.cpp
@@ -0,0 +1,60 @@
+#include "Stack.h"
+
+/**
+   Complete the member functions below
+ */
+
+Stack::Stack()
+{
+    // TODO
+}
+
+Stack::Stack(const Stack &s)
+{
+    // TODO
+}
+
+Stack::~Stack()
+{
+    // TODO
+}
+    
+bool Stack::isEmpty() const
+{
+    // TODO
+    return false;
+}
+
+int Stack::top() const 
+{
+    // TODO
+    return 0;
+}
+
+void Stack::pop()
+{
+    // TODO
+}
+
+void Stack::push(int elem)
+{
+    // TODO
+}
+
+void Stack::clear()
+{
+    // TODO
+}
+
+int Stack::size() const
+{
+    // TODO
+    return 0;
+}
+
+int Stack::maxsize() const
+{
+    // TODO
+    return 0;
+}
+
