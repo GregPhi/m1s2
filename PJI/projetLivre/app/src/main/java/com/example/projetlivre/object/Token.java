1d2e8cbfbbd8d2ebd47e5a47c5765b553f7e9c4b
GregPhi <gregoire.philippot@laposte.net>
2020-04-30 00:55:34 +0200

    gitignore

--- PJI/projetLivre/app/src/main/java/com/example/projetlivre/object/Token.java
+++
@@ -1,6 +0,0 @@
-package com.example.projetlivre.object;
-
-public class Token {
-    public final static String mailTestFrom = "phigreg96@gmail.com";
-    public final static String mailTestSend = "lordlong59@gmail.com";
-}
